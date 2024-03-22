157dc16c1c532a4fc144952b8e457d6e4ad9ecc0
licoma <lcm413453@gmail.com>
2024-03-22 09:10:33 +0900

    "2024-03-22-09:10"

--- basic/src/main/java/com/changmin/basic/dto/SampleDto.java
+++ basic/src/main/java/com/changmin/basic/dto/SampleDto.java
@@ -1,10 +1,15 @@
 package com.changmin.basic.dto;
 
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,9 +36,37 @@ public class SampleDto {
     @NotBlank       
     private String notBlank;
 
+    // RequestBody로 받아오는 데이터는 Wrapper 클래스 타입으로 데이터를 받는 것이 좋음
+    @NotNull
+    //@NotEmpty @NotBlank 는 문자열에서만 유효함
+    //@NotEmpty @NotBlank 
+    private Integer requiredInt;
+
     // 문자열일 경우 문자열 길이의 최소 최대를 지정 (null 값이면 검사 안함)
     @Length(min=5, max=10)
     @NotNull
     private String length;
+
+    @NotNull
+    // 최댓값과 최솟값을 지정
+    @Max(100)
+    @Min(0)
+    private Integer maxMin;
+
+    @NotNull
+    // 최대 최소 범위를 지정
+    @Range(min=0, max=100)
+    private Integer range;
+
+    @NotEmpty
+    // 문자열이 이메일 형식인지 확인
+    @Email
+    private String email;
+
+    
+    // 정규식을 이용해 문자열의 패턴 검사를 진행
+    // email@email.com | email@email.co.kr
+    @Pattern(regexp="^[a-zA-Z0-9]*@([-.]?[a-zA-Z0-9])*\\.[a-zA-Z]{2,4}$")
+    private String emailPattern;
 
 }
