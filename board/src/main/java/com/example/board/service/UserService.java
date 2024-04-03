e36003eb1e60f93c6bae5f4d36d76963763f71f5
licoma <lcm413453@gmail.com>
2024-04-03 14:07:32 +0900

    "2024-04-03_14:07"

--- board/src/main/java/com/example/board/service/UserService.java
+++ board/src/main/java/com/example/board/service/UserService.java
@@ -1,5 +1,11 @@
 package com.example.board.service;
 
+import org.springframework.http.ResponseEntity;
+
+import com.example.board.dto.response.user.GetUserResponseDto;
+
 public interface UserService {
     
+    ResponseEntity<? super GetUserResponseDto> getUser(String email);
+
 }
